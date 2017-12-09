4292f04e0d6bd8e49235816e05ea4cccf6a7b009
Alexander Trifonov <mobious96@gmail.com>
2017-12-09 07:56:21 +0300

    dijkstra

--- Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra.cpp
@@ -1,6 +1,16 @@
 #include "Graph.h"
 #include <algorithm>
-int DijkstraMark(Graph G, int node_start, int node_finish)
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+string IntToString(int a)
+{
+	ostringstream temp;
+	temp << a;
+	return temp.str();
+}
+int* DijkstraMark(Graph G, int node_start)
 {
 	//Initialization
 	int size = G.graph.size();
@@ -44,24 +54,136 @@ int DijkstraMark(Graph G, int node_start, int node_finish)
 		else
 			break;
 	}
-	return dist[node_finish];
+	delete[] labels;
+	return dist;
+}
+
+
+int* DijkstraHeap(Graph G, int node_start, int heap_size)
+{
+	DHeap tree(G.size, 9);
+	int* dist = new int[G.size];
+	for (int i = 0; i < G.size; i++)
+	{
+		dist[i] = INT_MAX;
+		tree.weight[i].key = i;
+		tree.weight[i].value = INT_MAX;
+	}
+	int ind;
+	tree.weight[node_start].value = 0;
+	tree.Queue();
+	Pair res;
+	while (tree.n > 0)
+	{
+		res = tree.GetMin(); //root;
+		int tmp;
+		tmp = res.key;
+		dist[tmp] = res.value;
+
+
+		for (int i = 0; i < G.graph[tmp].size(); i++)
+		{
+			Pair edge = G.graph[tmp][i];
+			if (edge.value + dist[tmp] < tree.weight[tree.index[edge.key]].value)
+			{
+				tree.weight[tree.index[edge.key]].value = edge.value + dist[tmp];
+				tree.Emersion(tree.index[edge.key]);
+			}
+		}
+	}
+	return dist;
 }
 
 void main()
 {
+	ofstream output1;
+	ofstream output2;
 	Graph G;
-	for (int i = 0; i < 5; i++)
-	{
-		G.Add(i);
-	}
-	vector<Pair> tmp = { {1,1},{2,3},{3,1} };
-	G.Link(0, tmp); //connect 0_node to 1_node with weight = 1, to 2_node with weight = 3, to 3_node with weight = 1;
-	tmp = { { 2,2 },{ 3,1 },{ 4,1 } };
-	G.Link(1, tmp);
-	tmp = { {4,2} };
-	G.Link(2, tmp);
-	G.Print();
+	clock_t time;
+	int step = 100;
+	int size;
 	int start = 0;
-	int finish = 4;
-	cout << "Shortest from "<<start<<" to "<<finish<<" is " << DijkstraMark(G, start, finish) << endl;
+	int* dist1;
+	int* dist2;
+
+	output1.open("output1_l.txt");
+	output2.open("output1_h.txt");
+	cout << "[ Generating full graph ]" << endl;
+
+	for (int i = 10; i < 10000; i = i + step)
+	{
+		size = 1000;
+		Graph G1;
+		G1.Generate(G1, size, pow(size, 2)/10, 10);
+		time = clock();
+		dist1 = DijkstraMark(G1, start);
+		output1 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+
+		time = clock();
+		dist2 = DijkstraHeap(G1, start, 9);
+		output2 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+	}
+	output1.close();
+	output2.close();
+
+	output1.open("output2_l.txt");
+	output2.open("output2_h.txt");
+	cout << "[ Generating linear graph ]" << endl;
+
+	for (int i = 100; i < 5000; i = i+step)
+	{
+		size = i;
+		Graph G2;
+		G2.Generate(G2, size, size * 3, 10);
+		time = clock();
+		dist1 = DijkstraMark(G2, start);
+		output1 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+
+
+		time = clock();
+		dist2 = DijkstraHeap(G2, start, 9);
+		output2 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+	}
+	output1.close();
+	output2.close();
+
+	output1.open("output3_l.txt");
+	output2.open("output3_h.txt");
+	cout << "[ Generating log graph ]" << endl;
+	for (int i = 100; i < 2500; i = i+ step)
+	{
+		size = i;
+		Graph G3;
+		time_t time_generate;
+		time_generate = clock();
+		G3.Generate(G3, size, pow(size, 2), 10);
+		time = clock();
+		dist1 = DijkstraMark(G3, start);
+		output1 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+		time = clock();
+		dist2 = DijkstraHeap(G3, start, 9);
+		output2 << i << " " << float(clock() - time) / CLOCKS_PER_SEC * 1000 << endl;
+	}
+	output1.close();
+	output2.close();
+
+
+	//for (int i = 0; i < 4; i++)
+	//	cout << dist2[i] << endl;
+
+	/*bool Equal = true;
+	for (int i = 0; i < size; i++)
+	{
+		if (dist1[i] != dist2[i])
+			Equal == false;
+	}
+	if (Equal)
+	{
+		cout << "Output: labels == heap" << endl;
+	}
+	else
+	{
+		cout << "Output: labels != heap" << endl;
+	}*/
+		//cout << "Shortest from " << start << " to " << finish << " is " << DijkstraHeap(G, start, finish,9) << endl;
 }
--- Dijkstra/Graph.h
+++ Dijkstra/Graph.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 using namespace std;
 struct Pair
 {
@@ -13,23 +16,171 @@ struct Pair
 		key = _key;
 		value = _value;
 	}
+
+	Pair& operator=(Pair& src)
+	{
+		key = src.key;
+		value = src.value;
+		return *this;
+	}
 };
+
+struct DHeap
+{
+	int n;
+	int d;
+	Pair* weight;
+	int* index;
+
+	int GetIndex(int i)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (weight[i].key == i)
+				return i;
+		}
+		return -1;
+	}
+
+	DHeap(int _n, int _d)
+	{
+		n = _n;
+		d = _d;
+		weight = new Pair[n];
+		index = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			index[i] = i;
+		}
+	}
+	~DHeap()
+	{
+		delete[] weight;
+		delete[] index;
+	}
+	int FirstChild(int i)
+	{
+		int j = i*d + 1;
+		if (j <= n - 1)
+			return j;
+		else
+			return -1;
+	}
+
+	int LastChild(int i)
+	{
+		int j = FirstChild(i);
+		if (j == -1)
+			return -1;
+		else
+			return min(j + d - 1, n - 1);
+	}
+
+	int Father(int i)
+	{
+		return (i-1)/d;
+	}
+
+	Pair MinChild(int i)
+	{
+		Pair tmp;
+		int kf = FirstChild(i);
+		if (kf == -1)
+		{
+			tmp.key = -1;
+			tmp.value = -1;
+			return tmp;
+		}
+		else
+		{
+			int kl = LastChild(i);
+			tmp.key = kf;
+			tmp.value = weight[kf].value;
+			for (int j = kf + 1; j < kl; j++)
+			{
+				if (weight[j].value < tmp.value)
+				{
+					tmp.value = weight[j].value;
+					tmp.key = j;
+				}
+			}
+			return tmp;
+		}
+	}
+	void Swap(int frs, int sec)
+	{
+		Pair buf = weight[frs];
+		weight[frs] = weight[sec];
+		weight[sec] = buf;
+		int fi = weight[frs].key;
+		int si = weight[sec].key;
+		int ibuf = index[fi];
+		index[fi] = index[si];
+		index[si] = ibuf;
+	}
+	void Diving(int i)
+	{
+		Pair j1(i, weight[i].value);
+		Pair j2 = MinChild(j1.key);
+		while ((j2.key != -1) && (j1.value > j2.value))
+		{
+			Swap(j1.key, j2.key);
+			j1 = j2;
+			j2 = MinChild(j1.key);
+		}
+	}
+	void Emersion(int i)
+	{
+		Pair j1(i, weight[i].value);
+		Pair j2(Father(j1.key),weight[Father(j1.key)].value);
+		while ((j1.key != 0) && (j1.value < j2.value))
+		{
+			Swap(j1.key, j2.key);
+			j1 = j2;
+			j2.key = Father(j1.key);
+			j2.value = weight[j2.key].value;
+		}
+	}
+	Pair GetMin()
+	{
+		Pair res = weight[0];
+		Swap(0, n - 1);
+		n = n - 1;
+		if(n > 1)
+			Diving(0);
+		return res;
+	}
+	void Queue()
+	{
+		for (int i = n - 1; i >= 0; i--)
+			Diving(i);
+	}
+
+};
+
+
 struct Graph
 {
 	vector<vector<Pair> > graph;
+	int size;
 
-	Graph() {};
+	Graph() { size = 0; };
 	Graph(Pair root)
 	{
 		graph = { {root} };
+		size = 1;
 	}
-
+	~Graph()
+	{
+		graph.clear();
+	}
 	void Add(int node)
 	{
 		vector<Pair> tmp;
 		Pair ntmp(node, -1);
 		tmp.push_back(ntmp);
 		graph.push_back(tmp);
+		size = size++;
 	}
 	void Link(int node, vector<Pair> toNodes)
 	{
@@ -46,6 +197,18 @@ struct Graph
 			if (graph[node][0].value == -1)
 				graph[node].pop_back();
 			graph[node].push_back(tmp);
+		}
+	}
+	void SetEdge(int node_1, int node_2, int weight)
+	{
+		if (GetValue(node_1, node_2) == -1)
+		{
+			if (graph[node_1][0].value == -1)
+				graph[node_1].pop_back();
+			graph[node_1].push_back(Pair(node_2, weight));
+			if (graph[node_2][0].value == -1)
+				graph[node_2].pop_back();
+			graph[node_2].push_back(Pair(node_1, weight));
 		}
 	}
 	void Print()
@@ -69,5 +232,31 @@ struct Graph
 				return graph[node_1][i].value;
 		}
 		return -1;
+	}
+
+	void Generate(Graph& G, int size, int edge_numbers, int max_weight)
+	{
+		int m = edge_numbers;
+		srand(time(0));
+		for (int i = 0; i < size; i++)
+		{
+			G.Add(i);
+		}
+		while (m)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				SetEdge(i, min(i + rand() % size, size - 1), rand() % max_weight + 1);
+				m = m--;
+				/*for (int j = i + 1; j < size; j++)
+				{
+					if ((rand() % 2 == 1) && (m != 0))
+					{
+						SetEdge(i, j, rand() % max_weight + 1);
+						m = m--;
+					}
+				}*/
+			}
+		}
 	}
 };
